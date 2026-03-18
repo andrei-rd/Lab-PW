3275f2b14ac5a1fc78307e02f8848220daa0d643
Andrei Roba <andrei.roba@student.unitbv.ro>
2026-03-18 15:11:49 +0200

    Laborator Laborator 5: React (QuickNote, TodoList, ContactForm, Clock)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-// Importul componentelor create în exercițiile 1-5
-import QuickNote from './QuickNote'; // Exercițiul 1 [cite: 27]
-import TodoList from './TodoList';   // Exercițiul 2 & 3 [cite: 71]
-import ContactForm from './ContactForm'; // Exercițiul 4 [cite: 95]
-import Clock from './Clock'; // Exercițiul 5 Bonus [cite: 109]
+import QuickNote from './QuickNote'; 
+import TodoList from './TodoList';   
+import ContactForm from './ContactForm';
+import Clock from './Clock'; 
 
 function App() {
   return (
@@ -13,23 +12,19 @@ function App() {
       
       <hr />
 
-      {/* Exercițiul 5: Ceas live care se actualizează în fiecare secundă [cite: 109] */}
       <section style={{ marginBottom: '30px' }}>
         <h2>Ora Curentă</h2>
         <Clock />
       </section>
 
-      {/* Exercițiul 1: Input controlat pentru o notă rapidă [cite: 5, 28] */}
       <section style={{ marginBottom: '30px' }}>
         <QuickNote />
       </section>
 
-      {/* Exercițiul 2 & 3: Listă de activități cu adăugare și ștergere [cite: 42, 75] */}
       <section style={{ marginBottom: '30px' }}>
         <TodoList />
       </section>
 
-      {/* Exercițiul 4: Formular de contact cu validare și feedback [cite: 92] */}
       <section style={{ marginBottom: '30px' }}>
         <ContactForm />
       </section>
