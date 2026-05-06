8178e52b2bd89bd91bf5ec47416fe9390e3dbc41
Andrei Roba <andrei.roba@student.unitbv.ro>
2026-05-06 15:21:19 +0300

    Ex 1: Activare CORS pe server

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Project = require('./models/Project');
 
+const cors = require('cors');
+
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/dashboard')
